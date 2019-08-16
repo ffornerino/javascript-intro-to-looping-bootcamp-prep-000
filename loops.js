a153f92364b11f236d6a99c70277ffe65f56ada5
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-16 14:31:56 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+var strangeloop = [];
+strangeloop.length = 25;
+
+ function forLoop(strangeloop){
+  for (var i = 0; i < 25; i++){
+    if (i === 0) { 
+      strangeloop.push(`I am ${i} strange loops.`);
+    } else if (i === 1) { 
+       strangeloop.push(`I am 1 strange loop.`);
+      } else {
+         strangeloop.push(`I am ${i} strange loops.`);
+      }
+  }  return strangeloop;
+}
+ 
+
+ 
+ function whileLoop(n){
+   while (n > 0){
+     console.log(--n);
+    //  if (--n === 0){
+       //   console.log("done");}
+     }
+ }
